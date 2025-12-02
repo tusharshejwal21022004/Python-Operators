ed97b766439d779f903d43206fe6650707b97319
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-02 21:51:32 +0530

    Add files via upload

---
+++ 2-assignment.py
@@ -0,0 +1,47 @@
+#Assignment Operator (=,+=,-=,*=,&=,//+,**=)
+
+#Equal to:
+
+a=10
+print(a)
+
+# (+=) plus equal to
+
+a=10
+a=a+2  
+a+=2   # a=10+2  a=12
+
+# (-=) 
+
+a=10
+a-=2
+print(a)
+
+# (*=) 
+
+a=10
+a*=2  #a=10*2
+print(a)
+
+# (/=) 
+
+a=10
+a/=2   #a=10/2 =5
+print(a)
+
+
+# (&=)
+
+a=10
+a%=2  #a=a%2
+print(a)
+
+# (//=)
+a=17
+a//=4  #a=a//4 =4
+print(a)
+
+# (**=)
+a=2
+a**=2
+print(a)
