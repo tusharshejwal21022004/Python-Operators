a417f73c00b523ddb6b9f6580d56eacce4d1cbdb
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-02 21:51:19 +0530

    Add files via upload

---
+++ 1-arithmetic.py
@@ -0,0 +1,42 @@
+# Arithmetic Operations (+,-,*,/,%,//,**)
+
+a=10
+b=20
+res=a+b
+print(res)
+
+a=12
+b=10
+res=a-b
+print(res)
+
+a=10
+b=10
+res=a*b
+print(res)
+
+a=100
+b=10
+res=a/b
+print(res)
+
+#Modular
+a=12
+b=2
+res=a%b
+print(res)
+
+# // Floor division
+
+a=19
+b=5
+res=a//b
+print(res)
+
+
+# ** Exponential 
+
+a=2
+b=3
+res=a**b
+print(res)
