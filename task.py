5de2975e174ee1e08e01dd3b011b673d65b9a985
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-02 21:52:36 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,91 @@
+# # 1.WAP and calculate the area of the rectangle.
+
+# len=int(input("Enter the value of lenght: "))
+# wid=int(input("Enter the value of width: "))
+
+# res=len*wid
+# print(f"The area of rectangle is: {res}")
+
+# # 2.Calculate the area of square
+
+# side=int(input("Enter the side: "))
+
+# res=side*side
+
+# print(f"The area of square is: {res}")
+
+
+# # 3.Calculate the area of circle
+
+# radius=int(input("Enter the radius of circle: "))
+# pi=3.14
+
+# res=pi*(radius*radius)
+
+# print(f"The area of circle is {pi}")
+
+
+# # 4.Calculate the perimeter of square
+
+# side=int(input("Enter the value: "))
+
+# res=4*side
+
+# print(f"The perimeter of square is {res}")
+
+# 5.Calculate the perimeter of rectangle
+
+# len=int(input("Enter the lenghth : "))
+# wid=int(input("Enter the width : "))
+
+# res=2*(len+wid)
+
+# print(f"The perimeter of rectangle is {res}")
+
+# # 6.WAP and calculate circumference of circle? 
+# #C=2πr
+
+# rad=int(input("Enter the radius of circle: "))
+# pi=3.14
+
+# res=2*pi*rad
+
+# print(f"The circumference of circle is {res}")
+
+# 7.WAP and calculate Avg of 2 numbers, taken from users.
+
+# num1=int(input("Enter the first number: "))
+# num2=int(input("Enter the second number: "))
+
+# res=(num1+num2)/2
+
+# print(f"The average of two number is {res}")
+
+# # 8.WAP and calculate the area of triangle.
+
+# base=int(input("Enter the base of triangle: "))
+# height=int(input("Enter the height of triangle: "))
+
+# res=1/2*base*height
+
+# print(f"THe area of the triangle is: {res}")
+
+# 9.Take 3 input from user and print average.
+
+# input1=int(input("Enter the first value: "))
+# input2=int(input("Enter the second value: "))
+# input3=int(input("Enter the third value: "))
+
+# res=(input1+input2+input3)/3
+
+# print(f"The average of above three input is {res}")
+
+# # 10.Write a program to take two integer inputs from user and print sum and product of them.
+
+# num1=int(input("Enter the first value : "))
+# num2=int(input("Enter the second value : "))
+
+# sum= num1 + num2
+
+# product= num1 *num2
+# print(f"The sum of above is {sum} and the product is {product}")
