9a7867a3761385314fa9e945bec0e088df281125
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-02 21:52:10 +0530

    Add files via upload

---
+++ 5-Membership.py
@@ -0,0 +1,23 @@
+#Membership Operator (in,not in)
+#Membership Operator is used to check data is available in datatype or not.
+#It will return boolean output
+
+
+
+list1=[12,45,67,89,15]
+data=4
+print(data in list1)  #False
+
+list2=[89,56,76,"Jay",56]
+print("Jay" in list2 )   #True
+
+#not in operator:
+
+s="i am in pune"
+sub="mumbai"
+
+print(sub not in s)  #True
+
+
+list2=[89,56,76,"Jay",56]
+print("Jay" not in list2 )   #False
