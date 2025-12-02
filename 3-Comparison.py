8c28f2c56993195937c56039452c65f6a376a4c8
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-02 21:51:45 +0530

    Add files via upload

---
+++ 3-Comparison.py
@@ -0,0 +1,25 @@
+#Comparison Operators (<,>,<=,>=,==,!=)
+
+#It will give output in form on True and False.
+
+a=20
+b=18
+print(a>b)  #True
+
+print(a<b)  #False
+
+a=89
+b=56
+print(a>b) #True
+print(a>=b)  #True
+
+a=90
+b=67
+print(a<=b) #False
+print(a<b) #False
+print(a==b) #False
+
+a=88
+b=88
+print(a != b) #False
+print(a == b) #True
